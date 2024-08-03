721f710e8bf08f88b28ca3b826ab6f3eab9751dd
BZCCY520 <2874843882@qq.com>
2024-08-03 13:56:40 +0800

    Site updated: 2024-08-03 13:56:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/02/xir/"];function toRandomPost(){
+var posts=["2024/08/02/xir/","2024/08/01/生日快乐！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
