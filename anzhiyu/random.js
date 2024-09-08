2698d772db0a37568946789deb3d9c4ad1508d42
BZCCY520 <2874843882@qq.com>
2024-09-08 23:27:39 +0800

    Site updated: 2024-09-08 23:27:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/10/AI/","2024/08/04/yu/","2024/08/22/emo/","2024/08/02/xir/","2024/08/01/生日快乐！/","2024/08/22/视频/"];function toRandomPost(){
+var posts=["2024/08/22/emo/","2024/08/04/yu/","2024/09/08/测试api/","2024/08/02/xir/","2024/08/10/AI/","2024/08/22/视频/","2024/08/01/生日快乐！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
